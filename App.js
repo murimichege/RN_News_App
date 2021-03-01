a479df49d4bd58d4b60b8e188a9813c7d483bc18
murimichege <chegeeddie@gmail.com>
2021-03-01 20:59:36 +0300

    Hid the statusbar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainStackNavigator from './src/Navigation/MainNavigation'
-
+import {StatusBar} from 'react-native'
 export default function App() {
+  StatusBar.setHidden(true, 'none');
 
   return (
+    
     
       <MainStackNavigator/>
 
--- src/Navigation/Screens/HomeScreen.js
+++ src/Navigation/Screens/HomeScreen.js
@@ -1,36 +1,48 @@
 import React from 'react'
-import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native'
-
+import {View, Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native'
+import HealthScreen from '../../Navigation/Screens/HealthScreen'
 function HomeScreen() {
     return (
       <ScrollView>
       <View style={styles.feed}>
-    <View style={[styles.card, {backgroundColor: '#997794'}]}>
+    <View style={[styles.card, {backgroundColor: '#CCAAC7'}]}>
       <Text style={styles.CardText}>World</Text>
       </View>
-    <View style={[styles.card, {backgroundColor: '#339966'}]} 
+    <View style={[styles.card, {backgroundColor: '#80E6B3'}]} 
     >
-      <Text>Science</Text>
+      <Text style={styles.CardText}>Science</Text>
       </View>
-    <View style={[styles.card, {backgroundColor: '#996633'}]}>
-      <Text>
-        Health
-
-      </Text>
+    <View style={[styles.card, {backgroundColor: '#E6B380'}]}>
+    <TouchableOpacity
+        style={styles.CardText}
+        onPress={() => navigation.navigate('HealthScreen')}
+      >
+        <Text>Health</Text>
+      </TouchableOpacity>
       </View>
-    <View style={[styles.card, {backgroundColor: '#669933'}]}>
-      <Text>Sports</Text>
+    <View style={[styles.card, {backgroundColor: '#99CC66'}]}>
+      <Text style={styles.CardText}>Sports</Text>
       </View>
-      <View style={[styles.card, {backgroundColor: '#669933'}]}>
-      <Text>Sports</Text>
+      <View style={[styles.card, {backgroundColor: '#696863'}]}>
+      <Text style={styles.CardText}>Politics</Text>
       </View>
-      <View style={[styles.card, {backgroundColor: '#669933'}]}>
-      <Text>Sports</Text>
+      <View style={[styles.card, {backgroundColor: '#84a1c2'}]}>
+      <Text style={styles.CardText}>Business</Text>
       </View>
      
-      <View style={[styles.card, {backgroundColor: '#669933'}]}>
-      <Text>Sports</Text>
+      <View style={[styles.card, {backgroundColor: '#FFC1FF'}]}>
+      <Text style={styles.CardText}>Sports</Text>
       </View>
+      <View style={[styles.card, {backgroundColor: '#d69057'}]}>
+      <Text style={styles.CardText}>Sports</Text>
+      </View>
+      <View style={[styles.card, {backgroundColor: '#5790d6'}]}>
+      <Text style={styles.CardText}>Sports</Text>
+      </View>
+      <View style={[styles.card, {backgroundColor: '#d8ce5f'}]}>
+      <Text style={styles.CardText}>Sports</Text>
+      </View>
+
  </View>
  </ScrollView>
     )
@@ -51,14 +63,12 @@ feed: {
     textAlign: 'center',
     color: 'white',
     fontWeight: 'bold',
-    
-
-
+    alignItems: 'flex-end'
   },
  card: {
   width: '48%',
   height: '10%',
-
+  alignSelf: 'flex-start',
   aspectRatio: 1,
   borderRadius: 20,
   marginBottom: 16  
